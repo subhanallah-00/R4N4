8f38baa0d96710e15d3486cb547e3ed1a81f7b3d
Nobita <spammer993@outlook.com>
2024-11-06 09:47:13 +0600

    Update npx.js

--- scripts/commands/npx.js
+++ scripts/commands/npx.js
@@ -1,14 +1,14 @@
 const fs = require("fs");
 module.exports = {
   config:{
-	name: "😒",
+	name: "🙄",
         version: "1.0.1",
         prefix: false,
 	permssion: 0,
 	credits: "nayan", 
 	description: "Fun",
 	category: "no prefix",
-	usages: "😒",
+	usages: "🙄",
         cooldowns: 5, 
 },
 
@@ -16,9 +16,9 @@ handleEvent: function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
   const content = event.body ? event.body : '';
   const body = content.toLowerCase();
-	if (body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf("😒")==0) {
+	if (body.indexOf(" ")==0 || body.indexOf("😳")==0 || body.indexOf("👀")==0 || body.indexOf("🙄")==0) {
 		var msg = {
-				body: "এঁভাঁবেঁ তাঁকাঁসঁ নাঁ প্রেঁমেঁ পঁরেঁ যাঁবোঁ 😚🥀",
+				body: "এঁভাঁবেঁ তাঁকাঁসঁ নাঁ প্রেঁমেঁ পঁরেঁ যাঁবোঁ তোঁ 😚🥀",
 				attachment: fs.createReadStream(__dirname + `/Nayan/Mayabi.mp3`)
 			}
 			api.sendMessage( msg, threadID, messageID);
