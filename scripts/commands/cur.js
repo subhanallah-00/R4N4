75b301d63c182eab833c975f6abdaca08bbae8a2
Nobita <spammer993@outlook.com>
2024-11-06 17:55:13 +0600

    Add files via upload

---
+++ scripts/commands/cur.js
@@ -0,0 +1,46 @@
+module.exports.config = {
+  name: "cur",
+  version: "1.0.0",
+  permission: 0,
+  credits: "SK-SIDDIK-KHAN",
+  description: "Nombar 1 Chor",
+  prefix: true, 
+  category: "Picture", 
+  usages: "user",
+  cooldowns: 5,
+  dependencies: {
+    "request":"",
+    "fs-extra":"",
+    "axios":""
+  }
+};
+ 
+module.exports.circle = async (image) => {
+	  const jimp = global.nodemodule['jimp'];
+  	image = await jimp.read(image);
+  	image.circle();
+  	return await image.getBufferAsync("image/png");
+};
+ 
+module.exports.run = async ({ event, api, args, Users }) => {
+try {
+  const Canvas = global.nodemodule['canvas'];
+  const request = global.nodemodule["node-superfetch"];
+  const jimp = global.nodemodule["jimp"];
+  const fs = global.nodemodule["fs-extra"];
+  var path_toilet = __dirname+'/cache/damma.jpg'; 
+  var id = Object.keys(event.mentions)[0] || event.senderID;
+  const canvas = Canvas.createCanvas(500, 670);
+	const ctx = canvas.getContext('2d');
+	const background = await Canvas.loadImage('https://i.imgur.com/ES28alv.png');
+  
+	var avatar = await request.get(`https://graph.facebook.com/${id}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`);
+	avatar = await this.circle(avatar.body);
+	ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+	ctx.drawImage(await Canvas.loadImage(avatar), 48, 410, 111, 111);
+	const imageBuffer = canvas.toBuffer();
+	fs.writeFileSync(path_toilet,imageBuffer);
+	 api.sendMessage({attachment: fs.createReadStream(path_toilet, {'highWaterMark': 128 * 1024}), body: "╭──────•◈•──────╮\nমুরগির ডিম চুরি করতে গিয়া ধরা খাইসে_ 🐸👻\n╰──────•◈•──────╯"}, event.threadID, () => fs.unlinkSync(path_toilet), event.messageID);
+}
+catch(e) {api.sendMessage(e.stack, event.threadID )}
+  }
