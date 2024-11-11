9e82cb4cdf5bca5bfd041de4c1c38ccff9c7a1e8
Nobita <spammer993@outlook.com>
2024-11-11 11:23:26 +0600

    Update jail.js

--- scripts/commands/jail.js
+++ scripts/commands/jail.js
@@ -1,41 +1,47 @@
-module.exports = {
-  config: {
-    name: "jail",
-    version: "1.1",
-    author: "Samir Thakuri",
-    prefix: "true",
-    countdown: 5,
-    role: 0,
-    description: "Jail image",
-    category: "image",
-    guide: {
-      en: "{pn} @tag"
-    }
-  },
+const fs = require("fs");
+const DIG = require("discord-image-generation"); 
+const usersData = require("yourUserDataModule"); 
 
-  langs: {
-    vi: {
-      noTag: "Bạn phải tag người bạn muốn tù"
-    },
-    en: {
-      noTag: "You must tag the person you want to jail"
-    }
-  },
+module.exports.config = {
+  name: "jail",
+  version: "1.0.1",
+  permission: 0,
+  prefix: true,
+  credits: "SK-SIDDIK-KHAN",
+  category: "fun",
+  usages: "user",
+  cooldowns: 5,
+};
 
-  onStart: async function ({ event, message, usersData, args, getLang }) {
-    const uid1 = event.senderID;
-    const uid2 = Object.keys(event.mentions)[0];
-    if (!uid2)
-      return message.reply(getLang("noTag"));
+module.exports.handleEvent = async function({ api, event, client, __GLOBAL }) {
+  const uid1 = event.senderID;
+  const uid2 = Object.keys(event.mentions)[0];
+
+  if (!uid2) return api.sendMessage("Please tag a user to send them to jail!", event.threadID);
+
+  try {
     const avatarURL1 = await usersData.getAvatarUrl(uid1);
     const avatarURL2 = await usersData.getAvatarUrl(uid2);
+
     const img = await new DIG.Jail().getImage(avatarURL2);
     const pathSave = `${__dirname}/tmp/${uid2}_Jail.png`;
+
     fs.writeFileSync(pathSave, Buffer.from(img));
-    const content = args.join(' ').replace(Object.keys(event.mentions)[0], "");
-    message.reply({
-      body: `${(content || "সালা খুব তো মুরগি চুরির শখ থাক এখন জেল এ 😹!")} 🚔`,
-      attachment: fs.createReadStream(pathSave)
-    }, () => fs.unlinkSync(pathSave));
+
+    const content = event.body.replace(Object.keys(event.mentions)[0], "").trim() || "You're in jail! 🚔";
+
+    api.sendMessage(
+      {
+        body: content,
+        attachment: fs.createReadStream(pathSave),
+      },
+      event.threadID,
+      () => {
+        fs.unlinkSync(pathSave);
+      }
+    );
+  } catch (err) {
+    console.error("Error processing jail image:", err);
+    api.sendMessage("An error occurred while processing the request.", event.threadID);
   }
 };
