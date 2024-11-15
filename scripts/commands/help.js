888550778a5bff33e6238356a85eda2dbc282632
Nobita <spammer993@outlook.com>
2024-11-15 09:38:09 +0600

    Update help.js

--- scripts/commands/help.js
+++ scripts/commands/help.js
@@ -1,33 +1,36 @@
+const axios = require('axios');
+const jimp = require("jimp");
+const fs = require("fs");
 module.exports.config = {
- name: "help",
- version: "2.0.0",
- permssion: 0,
- credits: "Edit by Mohammad Rana",
-  prefix:true,
- description: "commands list",
- category: "system",
- usages: "module name",
- cooldowns: 1,
- envConfig: {
-  autoUnsend: false,
-  delayUnsend: 300
+  name: "help",
+  version: "1.0.2",
+  permission: 0,
+  credits: "NAYAN",
+  description: "beginner's guide",
+  prefix: true,
+  category: "guide",
+  usages: "[Shows Commands]",
+  cooldowns: 5,
+  envConfig: {
+    autoUnsend: true,
+    delayUnsend: 60
  }
 };
-
+ 
 module.exports.languages = {
  "en": {
-  "moduleInfo": "•—»✨  [ %1 ]  ✨«—•\n\nUsage: %3\nCategory: %4\nWaiting time: %5 seconds(s)\nPermission: %6\nDescription: %2\n\nModule coded by %7",
+  "moduleInfo": "•—»✨[ %1 ]✨«—•\n\nUsage: %3\nCategory: %4\nWaiting time: %5 seconds(s)\nPermission: %6\nDescription: %2\n\nModule coded by %7",
   "helpList": '[ There are %1 commands on this bot, Use: "%2help nameCommand" to know how to use! ]',
   "user": "User",
         "adminGroup": "Admin group",
         "adminBot": "Admin bot"
  }
 };
-
+ 
 module.exports.handleEvent = function ({ api, event, getText }) {
  const { commands } = global.client;
  const { threadID, messageID, body } = event;
-
+ 
  if (!body || typeof body == "undefined" || body.indexOf("help") != 0) return;
  const splitBody = body.slice(body.indexOf("help")).trim().split(/\s+/);
  if (splitBody.length == 1 || !commands.has(splitBody[1].toLowerCase())) return;
@@ -36,7 +39,7 @@ module.exports.handleEvent = function ({ api, event, getText }) {
  const prefix = (threadSetting.hasOwnProperty("PREFIX")) ? threadSetting.PREFIX : global.config.PREFIX;
  return api.sendMessage(getText("moduleInfo", command.config.name, command.config.description, `${prefix}${command.config.name} ${(command.config.usages) ? command.config.usages : ""}`, command.config.commandCategory, command.config.cooldowns, ((command.config.hasPermssion == 0) ? getText("user") : (command.config.hasPermssion == 1) ? getText("adminGroup") : getText("adminBot")), command.config.credits), threadID, messageID);
 }
-
+ 
 module.exports. run = function({ api, event, args, getText }) {
   const axios = require("axios");
   const request = require('request');
@@ -55,11 +58,11 @@ if (args[0] == "all") {
       else group.find(item => item.group.toLowerCase() == commandConfig.config.commandCategory.toLowerCase()).cmds.push(commandConfig.config.name);
     }
     group.forEach(commandGroup => msg += `💫 ${commandGroup.group.charAt(0).toUpperCase() + commandGroup.group.slice(1)} \n${commandGroup.cmds.join(' • ')}\n\n`);
-
+ 
     return axios.get('https://loidsenpaihelpapi.miraiandgoat.repl.co').then(res => {
     let ext = res.data.data.substring(res.data.data.lastIndexOf(".") + 1);
-      let admID = "100029115600589";
-
+      let admID = "100000350435589";
+     
       api.getUserInfo(parseInt(admID), (err, data) => {
       if(err){ return console.log(err)}
      var obj = Object.keys(data);
@@ -88,44 +91,44 @@ if (args[0] == "all") {
  if (!command) {
   const arrayInfo = [];
   const page = parseInt(args[0]) || 1;
-    const numberOfOnePage = 20;
+    const numberOfOnePage = 10;
     let i = 0;
     let msg = "";
-
+   
     for (var [name, value] of (commands)) {
       name += ``;
       arrayInfo.push(name);
     }
-
+ 
     arrayInfo.sort((a, b) => a.data - b.data);
-
+   
 const first = numberOfOnePage * page - numberOfOnePage;
     i = first;
     const helpView = arrayInfo.slice(first, first + numberOfOnePage);
-
-
-    for (let cmds of helpView) msg += `•—»✨  ${cmds}  ✨«—•\n`;
-const siu = `❥͜͡𖠣꙰ٜٜٜٜٜٜٜٜٜ̋̀̋̀̋̀̋̀̋̀̋̀⚀ـٰٖٖٖٖٖٜ۬ـٰٰٖٖٖٖٜ۬ـٰٰٰٖٖٖٜ۬ـٰٰٰٰٖٖٜ۬ـٰٰٰٰٰٖٜ۬ 𝗔𝘀𝘀𝗮𝗹𝗮𝗺𝘂𝗮𝗹𝗮𝗶𝗸𝘂𝗺ـٰٖٖٖٖٖٜ۬ـٰٰٖٖٖٖٜ۬ـٰٰٰٖٖٖٜ۬ـٰٰٰٰٖٖٜ۬ـٰٰٰٰٰٖٜ۬⁜ٜٜٜٜٜٜٜٜٜ͜͡❥꙰ \nPage ｢${page}/${Math.ceil(arrayInfo.length/numberOfOnePage)}｣`;
-
+ 
+   
+    for (let cmds of helpView) msg += `┃━➤ ${cmds} \n`;
+    const siu = `┏━[𝗟𝗶𝘀𝘁 𝗼𝗳 𝗖𝗼𝗺𝗺𝗮𝗻𝗱𝘀]━➣\n┃━➤ Name  ${global.config.BOTNAME}\n┃━➤Page ｢${page}/${Math.ceil(arrayInfo.length/numberOfOnePage)}｣\n┃━━━━━━━━━━━━━━━➢`;
+ 
     const randomText = [ "Even a small amount of alcohol poured on a scorpion will drive it crazy and sting itself to death."," The crocodile can't stick its tongue out.","The oldest known animal in the world is a 405-year-old male, discovered in 2007.","Sharks, like other fish, have their reproductive organs located in the ribcage.","The eyes of the octopus have no blind spots. On average, the brain of an octopus has 300 million neurons. When under extreme stress, some octopuses even eat their trunks.","An elephant's brain weighs about 6,000g, while a cat's brain weighs only approximately 30g.","Cats and dogs have the ability to hear ultrasound.","Sheep can survive up to 2 weeks in a state of being buried in snow.","The smartest pig in the world is owned by a math teacher in Madison, Wisconsin (USA). It has the ability to memorize worksheets multiplying to 12.","Statistics show that each rattlesnake's mating lasts up to ... more than 22 hours", "Studies have found that flies are deaf.","In a lack of water, kangaroos can endure longer than camels.","","Dogs have 4 toes on their hind legs and 5 toes on each of their front paws.","The average flight speed of honey bees is 24km/h. They never sleep.","Cockroaches can live up to 9 days after having their heads cut off.","If you leave a goldfish in the dark for a long time, it will eventually turn white.","The flying record for a chicken is 13 seconds.","The mosquito that causes the most deaths to humans worldwide is the mosquito.","TThe quack of a duck doesn't resonate, and no one knows why.","Sea pond has no brain. They are also among the few animals that can turn their stomachs inside out.","Termites are active 24 hours a day and they do not sleep. Studies have also found that termites gnaw wood twice as fast when listening to heavy rock music.","Baby giraffes usually fall from a height of 1.8 meters when they are born.", "A tiger not only has a striped coat, but their skin is also streaked with stripes.."," Vultures fly without flapping their wings.","Turkeys can reproduce without mating.","Penguins are the only birds that can swim, but not fly. Nor have any penguins been found in the Arctic."," The venom of the king cobra is so toxic that just one gram can kill 150 people.","The venom of a small scorpion is much more dangerous than the venom of a large scorpion.","The length of an oyster's penis can be so 'monstrous' that it is 20 times its body size!","Rat's heart beats 650 times per minute.","The flea can jump 350 times its body length. If it also possessed that ability, a human would be able to jump the length of a football field once.","The faster the kangaroo jumps, the less energy it consumes.","Elephants are among the few mammals that can't jump! It was also discovered that elephants still stand after death.","Spiders have transparent blood."," Snails breathe with their feet.","Some lions mate more than 50 times a day.","Chuột reproduce so quickly that in just 18 months, from just 2 mice, the mother can give birth to 1 million heirs.","Hedgehog floats on water.","Alex is the world's first African gray parrot to question its own existence: What color am I?.","The reason why flamingos are pink-red in color is because they can absorb pigments from the shells of shrimp and shrimp that they eat every day."," Owls and pigeons can memorize human faces", "Cows are more dangerous than sharks","The single pair of wings on the back and the rear stabilizer help the flies to fly continuously, but their lifespan is not more than 14 days.","With a pair of endlessly long legs that can be up to 1.5 m high and weigh 20-25 kg, the ostrich can run faster than a horse. In addition, male ostriches can roar like a lion.","Kangaroos use their tails for balance, so if you lift a Kangaroo's tail off the ground, it won't be able to jump and stand.","Tigers not only have stripes on their backs but also printed on their skin. Each individual tiger is born with its own unique stripe.","If you are being attacked by a crocodile, do not try to get rid of their sharp teeth by pushing them away. Just poke the crocodile in the eye, that's their weakness.","Fleas can jump up to 200 times their height. This is equivalent to a man jumping on the Empire State Building in New York.","A cat has up to 32 muscles in the ear. That makes them have superior hearing ability","Koalas have a taste that does not change throughout life, they eat almost nothing but .. leaves of the eucalyptus tree.","The beaver's teeth do not stop growing throughout its life. If you do not want the teeth to be too long and difficult to control, the beaver must eat hard foods to wear them down.","Animals living in coastal cliffs or estuaries have extremely weird abilities. Oysters can change sex to match the mating method.","Butterflies have eyes with thousands of lenses similar to those on cameras, but they can only see red, green, and yellow..","Don't try this at home, the truth is that if a snail loses an eye, it can recover.","Giraffes do not have vocal cords like other animals of the same family, their tongues are blue-black.","Dog nose prints are like human fingerprints and can be used to identify different dogs.",];
- const text = `\n╭╼|════════════════|╾╮\n\n𝗖𝗢𝗠𝗠𝗔𝗡𝗗 𝗟𝗜𝗦𝗧: ${arrayInfo.length}\n𝗕𝗢𝗧 𝗡𝗔𝗠𝗘: ${global.config.BOTNAME}\n𝗣𝗥𝗘𝗙𝗜𝗫  : ｢ ${global.config.PREFIX} ｣\n ✆ 𝗟𝗜𝗡𝗞 : wa.me/01752865115  \n ⓕ 𝗟𝗜𝗡𝗞 :https://facebook.com/RANA.IS.BUSY.OKAY \n\n╰╼|════════════════|╾╯ `; 
+ const text = `┃━━━━━━━━━━━━━━━➢\n┃━➤ ${global.config.BOTNAME}\n┃━➤Command List: ${arrayInfo.length}\n┃━➤Bot Name: ${global.config.BOTNAME}\n┃━➤Prefix: ${global.config.PREFIX}\n┃━➤https://facebook.com/RANA.IS.BUSY.OKAY\N┗━━[𝗥𝗔𝗡𝗔  𝗕𝗢𝗧]━━━➣`;
     var link = [
-"https://i.imgur.com/wd2FTg0.jpeg",
-      "https://i.imgur.com/wd2FTg0.jpeg",
-      "https://i.imgur.com/wd2FTg0.jpeg",
-      "https://i.postimg.cc/bwcpfQhz/images-2023-12-07-T014059-713.jpg",
-      "https://i.postimg.cc/NGnQb3C2/images-2023-12-07-T014218-944.jpg",
-
+"https://i.imgur.com/TkFJoqh.jpeg",      
+"https://i.imgur.com/TkFJoqh.jpeg",    
+"https://i.imgur.com/TkFJoqh.jpeg",    
+"https://i.imgur.com/TkFJoqh.jpeg",    
+"https://i.imgur.com/TkFJoqh.jpeg",
+"https://i.imgur.com/TkFJoqh.jpeg",
+"https://i.imgur.com/TkFJoqh.jpeg",
     ]
      var callback = () => api.sendMessage({ body: siu + "\n\n" + msg  + text, attachment: fs.createReadStream(__dirname + "/cache/loidbutter.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/loidbutter.jpg"), event.messageID);
     return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname + "/cache/loidbutter.jpg")).on("close", () => callback());
  }
 const leiamname = getText("moduleInfo", command.config.name, command.config.description, `${(command.config.usages) ? command.config.usages : ""}`, command.config.commandCategory, command.config.cooldowns, ((command.config.hasPermssion == 0) ? getText("user") : (command.config.hasPermssion == 1) ? getText("adminGroup") : getText("adminBot")), command.config.credits);
-
+ 
   var link = [
-"https://i.imgur.com/kgz8X9R.jpeg",
+"https://i.postimg.cc/28zrCYPW/received-2253672584826375.jpg",
   ]
     var callback = () => api.sendMessage({ body: leiamname, attachment: fs.createReadStream(__dirname + "/cache/loidbutter.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/loidbutter.jpg"), event.messageID);
     return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname + "/cache/loidbutter.jpg")).on("close", () => callback());
 };
-  
