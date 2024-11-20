df91d01773683f75e9823097cb7b10a76e10d2dd
ALHAMDULILLAH <spammer993@outlook.com>
2024-11-20 18:24:47 +0600

    Create npx khanki.js

---
+++ scripts/commands/npx khanki.js
@@ -0,0 +1,58 @@
+module.exports.config = {
+  name: "🤭",
+  version: "1.0.0",
+  permission: 0,
+  credits: "MR-RANA",
+  description: "",
+  prefix: true, 
+  category: "no prefix", 
+  usages: "🤭",
+  cooldowns: 5,
+  dependencies: {
+    "request": "",
+    "fs-extra": "",
+    "axios": ""
+  }
+};
+ 
+module.exports.handleEvent = async ({ api, event, Threads }) => {
+  if (event.body.indexOf("khanki") == 0) {
+    const axios = global.nodemodule["axios"];
+    const request = global.nodemodule["request"];
+    const fs = global.nodemodule["fs-extra"];
+    var link = ["https://i.imgur.com/OKIBwxk.mp4"];
+    var callback = () => api.sendMessage({
+      body: `_-খা*কি মা* কয়ডার লগে চো*স`,
+      attachment: fs.createReadStream(__dirname + "/cache/2024.mp4")
+    }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/2024.mp4"), event.messageID);
+    const timeStart = Date.now();
+    const PREFIX = config.PREFIX;
+    return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname + "/cache/2024.mp4")).on("close", () => callback());
+  }
+};
+ 
+module.exports.languages = {
+  "vi": {
+    "on": "Dùng sai cách rồi lêu lêu",
+    "off": "sv ngu, đã bão dùng sai cách",
+    "successText": `🧠`,
+  },
+  "en": {
+    "on": "on",
+    "off": "off",
+    "successText": "success!",
+  }
+};
+ 
+module.exports.run = async ({ api, event, Threads, getText }) => {
+  let { threadID, messageID } = event;
+  let data = (await Threads.getData(threadID)).data;
+  if (typeof data["🫠"] == "undefined" || data["🤗"] == true) data["🤗"] = false;
+  else data["🤗"] = true;
+  await Threads.setData(threadID, {
+    data
+  });
+  global.data.threadData.set(threadID, data);
+  api.sendMessage(`${(data["😛"] == false) ? getText("off") : getText("on")} ${getText("successText")}`, threadID, messageID);
+};
+ 
