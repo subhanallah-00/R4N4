386456714e75db99a720c2f1551f9b91744d96bf
ALHAMDULILLAH <spammer993@outlook.com>
2024-11-23 21:27:29 +0600

    Update marry.js

--- scripts/commands/marry.js
+++ scripts/commands/marry.js
@@ -46,7 +46,7 @@ async function makeImage({ one, two }) {
     
     let circleOne = await jimp.read(await circle(avatarOne));
     let circleTwo = await jimp.read(await circle(avatarTwo));
-    batgiam_img.resize(432, 280).composite(circleOne.resize(60, 60), 200, 23).composite(circleTwo.resize(60, 60), 136, 40);
+    batgiam_img.resize(926, 736).composite(circleOne.resize(60, 60), 200, 23).composite(circleTwo.resize(60, 60), 136, 40);
     
     let raw = await batgiam_img.getBufferAsync("image/png");
     
