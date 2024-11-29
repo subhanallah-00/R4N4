dca445c39c181961316aea208f9d2b0b182c7231
ALHAMDULILLAH <spammer993@outlook.com>
2024-11-29 21:05:14 +0600

    Update tikmp3.js

--- scripts/commands/tikmp3.js
+++ scripts/commands/tikmp3.js
@@ -1,4 +1,3 @@
-
 module.exports.config = {
   name: "tikmp3",
   version: "2.0.0",
@@ -24,12 +23,12 @@ module.exports.run = async function({ api, event, args }) {
   const fs = require("fs");
   const axios = require("axios");
   const request = require("request");
-  const { ytdown, ndown, tikdown, twitterdown } = require("nayan-media-downloader")
+  const { ytdown, ndown, tikdown, twitterdown } = require("rana-all-media-downloader")
   const prompt = args.join(" ");
   if (!args[0]) return api.sendMessage("[ ! ] Input link.", threadID, messageID);
 
   const content = args.join(" ");
-  if (!args[1]) api.sendMessage(`𝐃𝐎𝐖𝐍𝐋𝐎𝐀𝐃𝐈𝐍𝐆 𝗔𝗨𝗗𝗜𝗢 𝐅𝐎𝐑 𝐘𝐎𝐔\n\n𝐏𝐋𝐄𝐀𝐒𝐄 𝐖𝟖...`, event.threadID, (err, info) => setTimeout(() => { api.unsendMessage(info.messageID) }, 10000));
+  if (!args[1]) api.sendMessage(`┏━━━━━<𝗠𝗘𝗗𝗜𝗔>━━━━━━┓\n┃  𝗗𝗢𝗪𝗡𝗟𝗢𝗔𝗗𝗜𝗡𝗚 𝗩𝗜𝗗𝗘𝗢 \n┃  𝗙𝗢𝗥 𝗬𝗢𝗨 📥\n┃    ↻ ◁ || ▷ ↺\n┃  𝗣𝗟𝗘𝗔𝗦𝗦𝗘 𝗪𝗔𝗜𝗧\n┗━━━━[ 𝗥𝗔𝗡𝗔 𝗕𝗢𝗧 ]━━━━┛`, event.threadID, (err, info) => setTimeout(() => { api.unsendMessage(info.messageID) }, 10000));
 
  try {
   const res = await tikdown(`${content}`);
@@ -47,7 +46,7 @@ console.log(res)
     setTimeout(function() {
 
       return api.sendMessage({
-        body: `==[ 𝐌𝐔𝐒𝐈𝐂 𝐓𝐈𝐊𝐓𝐎𝐊 ]====\n━━━━━━━━━━━━━━\n\n💬 𝗧𝗶𝘁𝗹𝗲 𝗮𝘂𝗱𝗶𝗼: ${title}\n✒ 𝐍𝐈𝐂𝐊𝐍𝐀𝐍𝐄: ${author.nickname}\n⏱ 𝐓𝐢𝐦𝐞: ${duration} second\n\n⇆ㅤ ㅤ◁ㅤ ❚❚ ㅤ▷ ㅤㅤ↻`,
+        body: `┏━━<𝐌𝐔𝐒𝐈𝐂 𝐓𝐈𝐊𝐓𝐎𝐊 >━━┓\n┃𝗧𝗶𝘁𝗹𝗲 𝗮𝘂𝗱𝗶𝗼: ${title}\n┃𝐍𝐈𝐂𝐊𝐍𝐀𝐍𝐄: ${author.nickname}\n┃𝐓𝐢𝐦𝐞: ${duration} second\n┃⇆ㅤ ㅤ◁ㅤ ❚❚ ㅤ▷ ㅤㅤ↻\n┗━━━━[ 𝗥𝗔𝗡𝗔 𝗕𝗢𝗧 ]━━━━┛`,
         attachment: fs.createReadStream(__dirname + '/cache/tik.mp3')
       }, threadID, messageID)
     }, 5000)
