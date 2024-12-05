0979a6f0c6c3f223489df0b6aa17b0607298c090
ALHAMDULILLAH <spammer993@outlook.com>
2024-12-05 16:41:39 +0600

    Update uptime.js

--- scripts/commands/uptime.js
+++ scripts/commands/uptime.js
@@ -120,8 +120,8 @@ registerFont(__dirname + `/nayan/CaviarDreams.ttf`, {
     });
     ctx.textAlign = "start";
     ctx.font = "45px time";
-    ctx.fillText("@" + "www.xnxx.com169", 250, 515)
-    ctx.fillText("@" + "MOHAMMAD-NAYAN", 250, 575)
+    ctx.fillText("@" + "rana.is.busy.okay", 250, 515)
+    ctx.fillText("@" + "MOHAMMAD-RANA", 250, 575)
    //ctx.fillText("@" + "DVFB.VietLe.pro", 405, 750)
     ctx.restore();
     ctx.save();
@@ -129,7 +129,7 @@ registerFont(__dirname + `/nayan/CaviarDreams.ttf`, {
     const imageBuffer = canvas.toBuffer();
    fs.writeFileSync(pathImg, imageBuffer);
   return api.sendMessage({
-    body: `┃======{ 𝗨𝗣𝗧𝗜𝗠𝗘 𝗥𝗢𝗕𝗢𝗧 }======┃\n\n→ Bot worked  ${hours} hours ${minutes} minutes ${seconds} seconds \n•━━━━━━━━━━━━━━━━━━━━━━━━•\n➠ 𝗠𝗢𝗛𝗔𝗠𝗠𝗔𝗗 𝗡𝗔𝗬𝗔𝗡\n➠ Bo𝐭 Name: ${global.config.BOTNAME}\n➠ Bot Prefix: ${global.config.PREFIX}\n➠ Commands count: ${commands.size}\n➠ Total Users: ${global.data.allUserID.length}\n➠ Total thread: ${global.data.allThreadID.length}\n➠ CPU in use:: ${pidusage.cpu.toFixed(1)}%\n➠ RAM: ${byte2mb(pidusage.memory)}\n➠ Ping: ${Date.now() - timeStart}ms\n➠ Character ID𝐭: ${id}\n•━━━━━━━━━━━━━━━━━━━━━━━━•\n[ ${timeNow} ]`,
+    body: `┏━━━━━━━━━━━━━━━━━┓\n┃━➤ 𝗨𝗣𝗧𝗜𝗠𝗘 𝗥𝗢𝗕𝗢𝗧 ⏰ \n┗━━━━━━━━━━━━━━━━━┛\n\n→ Bot worked  ${hours} hours ${minutes} minutes ${seconds} seconds \n✢•━━━━━━━━━━━━━━━━━•✢\n┃➠ 𝗠𝗢𝗛𝗔𝗠𝗠𝗔𝗗 𝗥𝗔𝗡𝗔\n┃➠ Bo𝐭 Name: ${global.config.BOTNAME}\n┃➠ Bot Prefix: ${global.config.PREFIX}\n┃➠ Commands count: ${commands.size}\n┃➠ Total Users: ${global.data.allUserID.length}\n┃➠ Total thread: ${global.data.allThreadID.length}\n┃➠ CPU in use:: ${pidusage.cpu.toFixed(1)}%\n┃➠ RAM: ${byte2mb(pidusage.memory)}\n┃➠ Ping: ${Date.now() - timeStart}ms\n┃➠ Character ID𝐭: ${id}\n✢•━━━━━━━━━━━━━━━━━•✢\n[ ${timeNow} ]`,
     attachment: fs.createReadStream(pathImg)
   },
     event.threadID,
