0d801d532e60328ce06c98a76172335be24f0b26
ALHAMDULILLAH <spammer993@outlook.com>
2024-12-06 17:54:20 +0600

    Update ra boss.js

--- scripts/commands/ra boss.js
+++ scripts/commands/ra boss.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 module.exports = {
   config:{
-  name: "farhan",
+  name: "rana",
         version: "1.0.1",
         prefix: false,
   permssion: 0,
@@ -16,10 +16,10 @@ handleEvent: function({ api, event, client, __GLOBAL }) {
   var { threadID, messageID } = event;
   const content = event.body ? event.body : '';
   const body = content.toLowerCase();
-  if (body.indexOf("Farhan")==0 || body.indexOf("FARHAN")==0 || body.indexOf("ফারহান")==0 || body.indexOf("farhan")==0) {
+  if (body.indexOf("Rana")==0 || body.indexOf("RANA")==0 || body.indexOf("রানা")==0 || body.indexOf("rana")==0) {
     var msg = {
-        body: "ফা্ঁর্ঁহা্ঁন্ঁ এ্ঁখ্ঁন্ঁ খু্ঁব্ঁ বি্ঁজি্ঁ 🥰❤️🥀",
-        attachment: fs.createReadStream(__dirname + `/Nayan/boss.mp3`)
+        body: "__〲 রানা বস এখন বিজি আছে ' যা বলার আমাকে বলুন..!!😙",
+        attachment: fs.createReadStream(__dirname + `/Nayan/Rana.mp3`)
       }
       api.sendMessage( msg, threadID, messageID);
     api.setMessageReaction("🥰", event.messageID, (err) => {}, true)
